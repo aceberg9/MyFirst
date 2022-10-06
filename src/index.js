6eec53b4d164ac389fda3e1779efd0de408f5832
aceberg9 <57888158+aceberg9@users.noreply.github.com>
2022-10-06 17:57:48 +1000

    48

--- src/index.js
+++ src/index.js
@@ -8,29 +8,24 @@ import App from "./App";
 import {Provider} from "react-redux";
 
 
-let rerenderEntireTree = (state) => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={store}>
-                    <App
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App
 
-                        //state={state}
-                        //dispatch={store.dispatch.bind(store)}
-                        //store={store}
-                    />
-                </Provider>
-            </BrowserRouter>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
-rerenderEntireTree(store.getState())
-store.subscribe(() => {
-        let state = store.getState()
-        rerenderEntireTree(state)
-    }
-)
+                    //state={state}
+                    //dispatch={store.dispatch.bind(store)}
+                    //store={store}
+                />
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+
+
 //
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
